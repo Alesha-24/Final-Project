e43d37283f8667e3390ab520acb455bf860fa77d
Alesha-24 <59664172+Alesha-24@users.noreply.github.com>
2020-05-05 17:20:43 -0400

    Email service functionality

---
+++ app/aggregate.py
@@ -0,0 +1,10 @@
+
+#first set default parameters that will work when deploeyed to the server
+#set these by calling from the .env file 
+
+def get_nyt_data():
+#this basically gets the results from the app where we get the nyt data
+# or I cna aggregate it on that app - ATBD 
+def news_api_data():
+
+#maybe I only have the news api one work with a remote server and the nyt one works in both 
--- app/my_app.py
+++ app/my_app.py
@@ -64,73 +64,78 @@ def date_validation(date, months):
         print("Please ensure you are inputting only numeric values")
     return correct 
 
-print("Welcome to Metanoia. This is a customized news app, where you can get the latest news tailored to your interests and what you care about!")
-print("---------------------------")
-print("To get started we need some information from you...")
-print("---------------------------")
-name = input("What is your name? ")
-topic = input(f"Hi {name}! Please input a topic you want to learn more about: ")
-print("The search results will automatically return the most recent articles relevant to your chosen topic.") 
-print("---------------------------")
-print("Please wait, aggregating data...")
+if __name__ == "__main__":
 
-response = get_articles_1()
-hits, all_articles = process_request(response)
-
-while hits == 0:
-    print("I'm sorry, your search returned 0 results, please ensure to enter your topic of interest carefully!")  
-    topic = input("PLease input a topic you want to learn more about: ")   
+    print("Welcome to Metanoia. This is a customized news app, where you can get the latest news tailored to your interests and what you care about!")
+    print("---------------------------")
+    print("To get started we need some information from you...")
+    print("---------------------------")
+    name = input("What is your name? ")
+    topic = input(f"Hi {name}! Please input a topic you want to search the New York Times for: ")
+    print("The search results will automatically return the most recent articles relevant to your chosen topic.") 
+    #mention that it'll come up with articles that list the keywords - or find way to better filter
     print("---------------------------")
     print("Please wait, aggregating data...")
-    hits, all_articles = get_articles_1()
 
-#put this in a function 
-for x in all_articles:
-    print(x["headline"]["main"])
-    print(x["pub_date"])
-    print(x["abstract"])
-    print(x["web_url"])
+    response = get_articles_1()
+    hits, all_articles = process_request(response)
 
-date_filter = input("You can filter your results by date of publishing. Please enter 'yes' if you would like to filter the results by date, otherwise enter 'no': ")
-while date_filter == "Yes" or "yes":
-    print("Please format your dates in the following format YYYYMMDD, so for example: 24th August 2019 = 20190824")
-    print("---------------------------")
-    begin_date = input("Please enter a start date: ")
-    end_date = input("Please enter an end date: ")
-    if begin_date > end_date:
-        print("Error! Please ensure that your start date is before the end date you enter.")
-        date_filter = "Yes"
-    elif begin_date <= end_date:
-        correct = date_validation(begin_date,months) 
-        while correct == False:
-            begin_date =input("Error! Please enter a valid date: ")
-            correct = date_validation(begin_date, months)
-        correct = date_validation(begin_date,months) 
-        while correct == False:
-            end_date =input("Error! Please enter a valid date: ")
-            correct = date_validation(end_date, months)
+    while hits == 0:
+        print("I'm sorry, your search returned 0 results, please ensure to enter your topic of interest carefully!")  
+        topic = input("PLease input a topic you want to learn more about: ")   
         print("---------------------------")
-        print("Please wait, filtering results...")
-        response = get_articles_2()
-        _, all_articles = process_request(response)
-        for x in all_articles:
-            print(x["headline"]["main"])
-            print(x["pub_date"])
-            print(x["abstract"])
-            print(x["web_url"])
-        date_filter = "No"
-        break
-if date_filter == "No" or "no":
-    print("We hope you found that interesting!")
-    keep_going = input("Would you like to search the news for another topic that you're interested? Please enter 'yes' or 'no': ")
-    # if keep_going == "yes" or "Yes":
-        
-    # else:
-    #     print("Thank you for using Metanoia!")
+        print("Please wait, aggregating data...")
+        hits, all_articles = get_articles_1()
 
-#to do:
-#put whole thing in while loop to run again
- 
+    #put this in a function 
+    for x in all_articles:
+        print(x["headline"]["main"])
+        print(x["pub_date"])
+        print(x["abstract"])
+        print(x["web_url"])
+    
+    #this should only run if in development mode - in product mode, just return latest news - ie 2020
+    date_filter = input("You can filter your results by date of publishing. Please enter 'yes' if you would like to filter the results by date, otherwise enter 'no': ")
+    while date_filter == "Yes" or "yes":
+        print("Please format your dates in the following format YYYYMMDD, so for example: 24th August 2019 = 20190824")
+        print("---------------------------")
+        begin_date = input("Please enter a start date: ")
+        end_date = input("Please enter an end date: ")
+        if begin_date > end_date:
+            print("Error! Please ensure that your start date is before the end date you enter.")
+            date_filter = "Yes"
+        elif begin_date <= end_date:
+            correct = date_validation(begin_date,months) 
+            while correct == False:
+                begin_date =input("Error! Please enter a valid date: ")
+                correct = date_validation(begin_date, months)
+            correct = date_validation(begin_date,months) 
+            while correct == False:
+                end_date =input("Error! Please enter a valid date: ")
+                correct = date_validation(end_date, months)
+            print("---------------------------")
+            print("Please wait, filtering results...")
+            response = get_articles_2()
+            _, all_articles = process_request(response)
+            for x in all_articles:
+                print(x["headline"]["main"])
+                print(x["pub_date"])
+                print(x["abstract"])
+                print(x["web_url"])
+            date_filter = "No"
+            break
+    #this should only run if its in development mode not production mode 
+    if date_filter == "No" or "no":
+        print("We hope you found that interesting!")
+        keep_going = input("Would you like to search the news for another topic that you're interested? Please enter 'yes' or 'no': ")
+        # if keep_going == "yes" or "Yes":
+            
+        # else:
+        #     print("Thank you for using Metanoia!")
+
+    #to do:
+    #put whole thing in while loop to run again
+    
 
 
 
